894b8a7b30fe092db9c69e9bc730a09857dd7ab5
Tim Gillons <timgillons@gmail.com>
2014-12-17 21:13:52 -0500

    Adding my first Xcode project

---
+++ Classwork/My first app/My first app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My first app
+//
+//  Created by Tim Gillons on 12/17/14.
+//  Copyright (c) 2014 Tim Gillons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
