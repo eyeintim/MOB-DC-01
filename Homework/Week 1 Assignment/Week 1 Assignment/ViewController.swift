bed22edbe18a8d52c2b7efba97f2be216c456557
Tim Gillons <timgillons@gmail.com>
2014-12-22 21:21:46 -0500

    Week two classwork

---
+++ Homework/Week 1 Assignment/Week 1 Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week 1 Assignment
+//
+//  Created by Tim Gillons on 12/22/14.
+//  Copyright (c) 2014 Tim Gillons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
